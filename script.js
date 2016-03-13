ddea530db5749b5e1ab0317f1e2e8f81827af4c0
Veronica Moreno <frau.veronika@gmail.com>
2016-03-12 23:02:49 -0800

    firebase keys

---
+++ script.js
@@ -0,0 +1,17 @@
+var beerRef = new Firebase('https://alexs-ales.firebaseio.com/');
+
+var soundBrewery = beerRef.child('Sound-Brewery');
+var loveCraft = beerRef.child('LoveCraft-Brewery');
+
+soundBrewery.set({location: 'Poulsbo', site: 'http://soundbrewery.com/'});
+loveCraft.set({location: 'Bremerton', site: 'http://www.lovecraftbrewing.com/'})
+
+var soundList = [['Humulo Nimbus', 'true'], ['Monk\'s Indiscretion', 'true'], ['Poundage Porter', 'true']];
+
+var soundBeers = soundBrewery.child('beers');
+
+for (var beer = 0; beer < soundList.length; beer++) {
+  soundBeers.child(soundList[beer][0]).set({isAvailable: soundList[beer][1]});
+}
+
+soundBeers.child('Poundage Porter').set({isAvailable: 'false'});
